384b1cde00aa48686d328667bd72ab45564f84ad
BubbleFish <daforsastudia@gmail.com>
2025-04-09 13:41:31 +0300

    refactor(HW3): Add @Override

--- src/me/bbfh/webapp/storage/ArrayStorage.java
+++ src/me/bbfh/webapp/storage/ArrayStorage.java
@@ -8,6 +8,7 @@ import java.util.Objects;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
+    @Override
     public void save(Resume resume) {
         if (this.size == this.CAPACITY) {
             System.err.printf("Trying to save() a resume `%s`, but the storage is at its full capacity %d\n", resume, this.CAPACITY);
@@ -20,6 +21,7 @@ public class ArrayStorage extends AbstractArrayStorage {
         this.storage[this.size++] = resume;
     }
 
+    @Override
     protected int find(String uuid) {
         for (int i = 0; i < this.size(); i++) {
             if (Objects.equals(this.storage[i].getUUID(), uuid)) {
