18771b2d7b365ed33916334b25084ab340fc5f86
BubbleFish <daforsastudia@gmail.com>
2025-04-10 19:43:22 +0300

    feat(HW4): Add constructors to Resume

--- src/me/bbfh/webapp/model/Resume.java
+++ src/me/bbfh/webapp/model/Resume.java
@@ -1,11 +1,21 @@
 package me.bbfh.webapp.model;
 
+import java.util.UUID;
+
 /**
  * Initial resume class
  */
 public class Resume implements Comparable<Resume> {
     // Unique identifier
     private String uuid;
+
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
 
     @Override
     public String toString() {
